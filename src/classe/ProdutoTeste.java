1972596e23db0b70d85d3ef6e23d83aa4d703a58
UelingtonJesus <uelingtondejesuss.uj@gmail.com>
2022-06-18 18:59:10 -0300

    second commit

---
+++ src/classe/ProdutoTeste.java
@@ -0,0 +1,18 @@
+package classe;
+
+public class ProdutoTeste {
+    public static void main(String[] args) {
+        Produto p1 = new Produto();
+        p1.nome="Caneta";
+        p1.preco = 25.6;
+
+        Produto p2 = new Produto("Notebook",1456.8);
+
+
+
+        double precoFinal1 = p1.produtoComDesconto();
+        double precoFinal2 = p2.produtoComDesconto();
+        System.out.printf(p1.nome+" Valor final R$%.2f", precoFinal1);
+        System.out.printf(p2.nome+" Valor final R$%.2f", precoFinal2);
+    }
+}
